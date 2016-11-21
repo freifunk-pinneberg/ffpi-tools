e9a63db7dda27e946bff7308c30c5c21e658740c
Thomas Hooge <pirat@hoogi.de>
2016-11-21 19:48:10 +0100

    Cronjob für die Vnstat-Statistik eingebaut

--- install-tools.sh
+++ install-tools.sh
@@ -3,5 +3,6 @@
 install -v fastd-status.py /usr/local/bin
 install -v ffgate-check /usr/local/bin
 install -v alfred-announce.py /usr/local/bin
+install -v vnstati.sh /usr/local/bin
 
 install -v init.d/fastd /etc/init.d
